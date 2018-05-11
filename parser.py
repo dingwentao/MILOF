00ff4af9197966eac8f328ab39d1c52913b076eb
dingwentao <ustc.dingwentao@gmail.com>
2018-05-11 15:31:26 -0400

    update parser

--- parser.py
+++ parser.py
@@ -1,7 +1,26 @@
 import adios as ad
-import getopt, sys
-import os
 
-filepath = "data/tau-metrics-updated/tau-metrics.bp"
-f = ad.file(filepath, method_name='BP', is_stream=True, timeout_sec = 10.0)
-f.printself()
+method = "BP"
+init = "verbose=3;"
+
+ad.read_init(method, parameters=init)
+
+f = ad.file("data/tau-metrics-updated/tau-metrics.bp", method, is_stream=True, timeout_sec = 10.0)
+
+i = 0
+while True:
+    print(">>> step:", i)
+    v = f.var['event_timestamps']
+    print(v)
+
+    val = v.read(nsteps=1)
+    print(val.shape)
+
+    print(">>> advance ... ")
+    if (f.advance() < 0):
+        break
+    i += 1
+
+f.close()
+
+print(">>> Done.")
