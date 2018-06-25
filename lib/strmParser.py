80ae0b6d4bdbe23fb12093c49e0f94b5d3d692e3
Dingwen Tao <guanyisun@Guanyis-MacBook-Air.local>
2018-06-25 11:29:54 -0400

    update

--- lib/strmParser.py
+++ lib/strmParser.py
@@ -25,6 +25,9 @@ import adios as ad
 import numpy as np
 import scipy.io as sio
 import configparser
+import MiLOF
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.neighbors import LocalOutlierFactor
 
 
 def Parser(configFile):
@@ -59,10 +62,14 @@ def Parser(configFile):
 		if attr_name[i].startswith('timer'):
 			num_func = num_func + 1
 			func_name.append(attr_value[i])
+		if attr_name[i].startswith('event_type'):
+			print(attr_value[i])
 	attr_name = np.array(attr_name)
 	func_name = np.array(func_name)
 
 	i = 0
+	total_timestep = 0
+	anomaly_indices = []
 	while True:
 		print(">>> step:", i)
 
@@ -77,10 +84,6 @@ def Parser(configFile):
 			data_step = data_event
 			# count most common functions
 			int_func = ct(data_event[:, 4]).most_common(int_func_num) # e.g., [(16, 14002), (15, 14000), (13, 6000),...]
-			# if i == 0:
-			# 	data_step = data_event
-			# else:
-			# 	data_step = np.concatenate((data_step, data_event), axis=0)
 
 		vname = "counter_values"
 		if vname in fin.vars:
@@ -104,13 +107,16 @@ def Parser(configFile):
 			data_comm[:, 11] = comm[:, 7]
 			data_step = np.concatenate((data_step, data_comm), axis=0)
 
-		print("Size of current timestep =", data_step.shape)
-
 		# sort data in this step by timestamp
 		data_step = data_step[data_step[:, 11].argsort()]
 
+		if i == 0:
+			data_global = data_step
+		else:
+			data_global = np.concatenate((data_global, data_step), axis=0)
+
 		# lauch anomaly detection
-		flag = False
+		anomaly_flag = False
 
 		# dynamic interest list
 		if len(int_func) < 3:
@@ -118,15 +124,38 @@ def Parser(configFile):
 		else:
 			print ("Most three interested functions:\n", func_name[int_func[0][0]], "\n", func_name[int_func[1][0]], "\n", func_name[int_func[2][0]])
 
-		# ...
+		# matching data
+		global_index = (np.arange(data_step.shape[0]) + total_timestep).reshape(data_step.shape[0], 1)
+		data_step = np.append(data_step, global_index, axis=1)
+		func_data = data_step[data_step[:, 4]==21] # 21 is adios_close, TODO
+		entry_data = func_data[func_data[:, 3]==0] # 0 is entry in the current data, TODO
+		exit_data  = func_data[func_data[:, 3]==1] # TODO
 
-		# simulate an anomaly
-		if i == 8:
-			flag = True
-		# ....
+		# generating streaming data in terms of one function
+		datastream = []
+		for j in range(0, entry_data.shape[0]):
+			for k in range(0, exit_data.shape[0]):
+				if np.array_equal(entry_data[j, 0:3], exit_data[k, 0:3]):
+					entry_time = entry_data[j,11]
+					exec_time = exit_data[k, 11] - entry_data[j, 11]
+					datastream += [[entry_time, exec_time]]
+					break
+		datastream = np.array(datastream)
+
+		# anomaly detection
+		if (datastream.shape[0]):
+			scaler = MinMaxScaler()
+			scaler.fit(datastream)
+			datastream = scaler.transform(datastream)
+			# Should call MILOF API, but here for simplicity, call LOF directly
+			clf = LocalOutlierFactor(algorithm="kd_tree", metric='euclidean')
+			anomalies = entry_data[clf.fit_predict(datastream)==-1]
+			if anomalies.shape[0]:
+				anomaly_indices.extend(anomalies[:, -1].tolist())
+				anomaly_flag = True
 
 		# add or dump queue
-		if flag:
+		if anomaly_flag:
 			# dump queue to file	
 			db.appendleft(attr_value)
 			db.appendleft(attr_name)
@@ -142,6 +171,9 @@ def Parser(configFile):
 			# add data to queue
 			db.extend(data_step)
 
+		print("Size of current timestep =", data_step.shape[0])
+		total_timestep += data_step.shape[0]
+		print("Size of total timestep = ", total_timestep)
 
 		print(">>> Advance to next step ... ")
 		if (fin.advance() < 0):
@@ -157,16 +189,23 @@ def Parser(configFile):
 	print(">>> Load data ...")
 	fin = open(prov_db_path, "rb")
 	db2 = pickle.load(fin)
-	print("**** Print info ****")
-	print("Number of attributes =", db2[0])
-	print("First 20 Names of attributes =", db2[1][0:20])
-	print("First 20 Values of attributes =", db2[2][0:20])
-	print("First 20 trace data =", np.array(list(itertools.islice(db2, 3, 20))))
+	print(">>> Passed test of deserialization.")
+
+	print("\n**** Print info ****")
+	print(">>> Number of attributes =", db2[0])
+	print(">>> First 20 Names of attributes =", db2[1][0:20])
+	print(">>> First 20 Values of attributes =", db2[2][0:20])
+	print(">>> First 20 trace data =", np.array(list(itertools.islice(db2, 3, 20))))
+	print(">>> Indices of anomalies in terms of entry:", anomaly_indices)
 	fin.close()
 	
-	# import json
-	# file_path = "data.json"
-	# data_step = data_step[data_step[:, 11].argsort()]
-	# with open('data.json', 'w') as outfile:
-	# 	json.dump(data_step.tolist(), outfile)
-	# outfile.close()
+	import json
+	file_path = "data.json"
+	with open(file_path, 'w') as outfile:
+		json.dump(data_global.tolist(), outfile)
+	outfile.close()
+	
+	file_path = "anomaly.json"
+	with open(file_path, 'w') as outfile:
+		json.dump(anomaly_indices, outfile)
+	outfile.close()
